b8996be538280c0f9032daf8e11dd544e6ec2339
dher405 <david.herron74@gmail.com>
2025-03-21 13:17:22 -0600

    Update Diagnostics.js

--- src/Diagnostics.js
+++ src/Diagnostics.js
@@ -212,15 +212,16 @@ const STUNWebSocketTest = () => {
 
   try {
     wsIQ.current = new WebSocket(wsUrl, [], {
-      headers: {
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-        "Sec-WebSocket-Version": "13",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits",
-      },
+        headers: {
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+            "Sec-WebSocket-Version": "13",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits",
+            "Origin": "https://ringcx.ringcentral.com", // Add origin header
+        },
     });
 
     wsIQ.current.onopen = () => {
